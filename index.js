f874b7bb8c6bec652236f86d4780fc4c3ffe9aa2
withnahidul <nahidulislam6800@gmail.com>
2022-05-28 19:38:50 +0600

    admin-profile

--- index.js
+++ index.js
@@ -95,12 +95,7 @@ async function run() {
             res.send(users);
         });
 
-        app.get('/admin/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = await userCollection.findOne({ email: email });
-            const isAdmin = user.role === 'admin';
-            res.send({ admin: isAdmin })
-        });
+
 
         app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
